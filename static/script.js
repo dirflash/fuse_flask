abed31e710d26a30272b61599996f75e988a4b61
Aaron Davis <aaron.e.davis@gmail.com>
2024-09-24 20:09:56 -0600

    Front-end formatting

---
+++ static/script.js
@@ -0,0 +1,18 @@
+// mobile menu
+const burgerIcon = document.querySelector('#burger');
+const navbarMenu = document.querySelector('#nav-links');
+
+burgerIcon.addEventListener('click', () => {
+  navbarMenu.classList.toggle('is-active');
+});
+
+// notification delete
+document.addEventListener('DOMContentLoaded', () => {
+    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+      const $notification = $delete.parentNode;
+  
+      $delete.addEventListener('click', () => {
+        $notification.parentNode.removeChild($notification);
+      });
+    });
+  });
